0c85b1cf58ff7a8ce1eccbbed3346dd627fb4ee3
sunqfuu <sagir.o@outlook.com>
2025-02-24 17:01:44 +0300

    Console Commands

---
+++ template.js
@@ -0,0 +1,33 @@
+console.log("external js")
+
+// Single Comment
+
+/*
+
+Multiple Comment
+Multiple Comment
+Multiple Comment
+
+*/
+
+//window.
+
+//document.
+
+console.log("console.log");
+
+//Log
+window.console.log("window.console.log");
+
+//Warn - Warning
+console.warn("console.warn");
+
+//Info - Information
+console.info("console.info");
+
+//Error
+console.error("console.error");
+
+//Alert
+// alert("Alert")
+// window.alert("window.alert")
