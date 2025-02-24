ed316ed4bbce1c98c3d499d7867ac617ba0afc2c
sunqfuu <sagir.o@outlook.com>
2025-02-24 17:41:33 +0300

    Variable, Let, Const

--- template.js
+++ template.js
@@ -1,4 +1,4 @@
-console.log("external js")
+// console.log("external js")
 
 // Single Comment
 
@@ -10,24 +10,53 @@ Multiple Comment
 
 */
 
-//window.
+// //window.
 
-//document.
+// //document.
 
-console.log("console.log");
+// console.log("console.log");
 
-//Log
-window.console.log("window.console.log");
+// //Log
+// window.console.log("window.console.log");
 
-//Warn - Warning
-console.warn("console.warn");
+// //Warn - Warning
+// console.warn("console.warn");
 
-//Info - Information
-console.info("console.info");
+// //Info - Information
+// console.info("console.info");
 
-//Error
-console.error("console.error");
+// //Error
+// console.error("console.error");
 
 //Alert
 // alert("Alert")
 // window.alert("window.alert")
+
+// Variable
+
+// Common Rules
+var name="sunqfu";
+console.log(name);
+
+var companyName="sunqfu";
+console.log(companyName44);
+
+var $_companyName="thomyork";
+console.log($_companyName44);
+
+//  Unwritable
+// var üğşçö~30$_companyName="thomyork"
+// console.log($30_companyName44)
+
+// Let
+let companyName2="sunqfu FrontEnd";
+console.warn(companyName2);
+companyName2="sunqfu FrontEnd Data"
+
+// You Can't Use
+// let companyName="sunqfu FrontEnd";
+// console.warn(companyName2)
+
+// Const
+const companyName3="sunqfu FrontEnd JS HTML5";
+console.error(companyName3);
