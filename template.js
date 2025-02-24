9caa0fc56d6a74eeaaa8c3d0c55d7ee0e4348ba0
sunqfuu <sagir.o@outlook.com>
2025-02-24 17:43:01 +0300

    Prettier

--- template.js
+++ template.js
@@ -1,62 +1,62 @@
-// console.log("external js")
+//  console.log("external js")
 
 // Single Comment
 
 /*
 
-Multiple Comment
-Multiple Comment
-Multiple Comment
+ Multiple Comment
+ Multiple Comment
+ Multiple Comment
 
-*/
+ */
 
-// //window.
+// window.
 
-// //document.
+// document.
 
-// console.log("console.log");
+//  console.log("console.log");
 
-// //Log
-// window.console.log("window.console.log");
+// Log
+//  window.console.log("window.console.log");
 
-// //Warn - Warning
+//Warn - Warning
 // console.warn("console.warn");
 
-// //Info - Information
-// console.info("console.info");
+// Info - Information
+//  console.info("console.info");
 
-// //Error
+// Error
 // console.error("console.error");
 
-//Alert
-// alert("Alert")
-// window.alert("window.alert")
+// Alert
+//  alert("Alert")
+//  window.alert("window.alert")
 
-// Variable
+//  Variable
 
-// Common Rules
-var name="sunqfu";
-console.log(name);
+//  Common Rules
+// var name="sunqfu";
+// console.log(name);
 
-var companyName="sunqfu";
-console.log(companyName44);
+// var companyName="sunqfu";
+// console.log(companyName44);
 
-var $_companyName="thomyork";
-console.log($_companyName44);
+// var $_companyName="thomyork";
+// console.log($_companyName44);
 
-//  Unwritable
-// var üğşçö~30$_companyName="thomyork"
-// console.log($30_companyName44)
+//   Unwritable
+//  var üğşçö~30$_companyName="thomyork"
+//  console.log($30_companyName44)
 
-// Let
-let companyName2="sunqfu FrontEnd";
-console.warn(companyName2);
-companyName2="sunqfu FrontEnd Data"
+//  Let
+// let companyName2="sunqfu FrontEnd";
+// console.warn(companyName2);
+// companyName2="sunqfu FrontEnd Data"
 
 // You Can't Use
-// let companyName="sunqfu FrontEnd";
-// console.warn(companyName2)
+//  let companyName="sunqfu FrontEnd";
+//  console.warn(companyName2)
 
-// Const
-const companyName3="sunqfu FrontEnd JS HTML5";
-console.error(companyName3);
+//  Const
+// const companyName3="sunqfu FrontEnd JS HTML5";
+// console.error(companyName3);
